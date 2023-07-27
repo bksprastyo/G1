3ed58840dd601e58321cdbfc8c9c3c94b8ce2ad3
BAYU <68054117+bksprastyo@users.noreply.github.com>
2023-07-27 14:48:02 +0400

    Add files via upload

---
+++ G1.py
@@ -0,0 +1,12 @@
+import random
+import sys
+import time
+def mengetik(s):
+   for c in s - '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+
+
+        time.sle(random.random()*0.3)
+
+mengetik('Jangan Lupa subcribe Chanel User Eror21 \nSelamat Menonton\nTerimakasih.')
